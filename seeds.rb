05011341284779b05214b4f96a7971d4b52217dd
Ryan Poplawski <claydewd@gmail.com>
2015-09-24 19:03:56 -0500

    making new seed file

---
+++ seeds.rb
@@ -0,0 +1,98 @@
+Employee.create({
+  name:       "James Leaps",
+  dept:       "Sporting Goods",
+  yrs_employed: 12,
+  position:   "Store Manager",
+  status:     "full-time",
+  pay:        "salary"
+  })
+
+Employee.create({
+  name:       "Ryan Erickson",
+  dept:       "Human Resources",
+  yrs_employed: 5,
+  position:   "Assistant_Manager",
+  status:     "part-time",
+  pay:        "hourly"
+  })
+
+Employee.create({
+  name:       "Nicole Stiles",
+  dept:       "Finance",
+  yrs_employed: 3,
+  position:   "Program Coordinator",
+  status:     "full-time",
+  pay:        "salary"
+  })
+
+Employee.create({
+  name:       "Mike Johnson",
+  dept:       "Sales",
+  yrs_employed: 7,
+  position:   "Marketing Specialist",
+  status:     "part-time",
+  pay:        "hourly"
+  })
+
+Employee.create({
+  name:       "Troy Sellers",
+  dept:       "Advertising",
+  yrs_employed: 1,
+  position:   "Regional Distribution Manager",
+  status:     "full-time",
+  pay:        "salary"
+  })
+
+Employee.create({
+  name:       "Kyle Makison",
+  dept:       "Web Design",
+  yrs_employed: 3,
+  position:   "Production Manager",
+  status:     "full-time",
+  pay:        "salary"
+  })
+
+Employee.create({
+  name:       "Michelle Jacobs",
+  dept:       "Engineering",
+  yrs_employed: 2,
+  position:   "ASIC Design Manager",
+  status:     "full-time",
+  pay:        "salary"
+  })
+
+Employee.create({
+  name:       "Jason Riles",
+  dept:       "Education",
+  yrs_employed: 9,
+  position:   "Math Teacher",
+  status:     "full-time",
+  pay:        "salary"
+  })
+
+Employee.create({
+  name:       "Alex Rivas",
+  dept:       "Maintenance",
+  yrs_employed: 2,
+  position:   "Cooling Specialist",
+  status:     "part-time",
+  pay:        "hourly"
+  })
+
+Employee.create({
+  name:       "Mary Runas",
+  dept:       "Research and Development",
+  yrs_employed: 5,
+  position:   "Chip Bring-up",
+  status:     "full-time",
+  pay:        "salary"
+  })
+
+#----------------------------
+### vacation info assigned to
+
+
+
+
+
+
