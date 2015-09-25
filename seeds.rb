66dbd6e9f141038a7c348668cc88899c5ff1c760
Ryan Poplawski <claydewd@gmail.com>
2015-09-24 21:43:29 -0500

    added vacation to seeds.rb file

--- seeds.rb
+++ seeds.rb
@@ -37,7 +37,7 @@ Employee.create({
 Employee.create({
   name:       "Troy Sellers",
   dept:       "Advertising",
-  yrs_employed: 1,
+  yrs_employed: 5,
   position:   "Regional Distribution Manager",
   status:     "full-time",
   pay:        "salary"
@@ -73,7 +73,7 @@ Employee.create({
 Employee.create({
   name:       "Alex Rivas",
   dept:       "Maintenance",
-  yrs_employed: 2,
+  yrs_employed: 8,
   position:   "Cooling Specialist",
   status:     "part-time",
   pay:        "hourly"
@@ -82,17 +82,78 @@ Employee.create({
 Employee.create({
   name:       "Mary Runas",
   dept:       "Research and Development",
-  yrs_employed: 5,
+  yrs_employed: 15,
   position:   "Chip Bring-up",
   status:     "full-time",
   pay:        "salary"
   })
 
-#----------------------------
-### vacation info assigned to
+Employee.create({
+  name:       "Kyle Makison",
+  dept:       "Web Design",
+  yrs_employed: 3,
+  position:   "Production Manager",
+  status:     "full-time",
+  pay:        "salary"
+  })
 
+Employee.create({
+  name:       "Mike Roberts",
+  dept:       "Administration",
+  yrs_employed: 7,
+  position:   "Administrative Assisitant",
+  status:     "full-time",
+  pay:        "salary"
+  })
 
+Employee.create({
+  name:       "Justin Micheals",
+  dept:       "Staffing",
+  yrs_employed: 9,
+  position:   "Recruiter",
+  status:     "part-time",
+  pay:        "hourly"
+  })
 
+Employee.create({
+  name:       "James Fransisco",
+  dept:       "Hardware Engineering",
+  yrs_employed: 12,
+  position:   "Circuit Designer",
+  status:     "full-time",
+  pay:        "salary"
+  })
 
+#################### vacation info assigned to:
+15.times do Vacaion.create({date_taken: Faker::Date.between(Date.now - 14, Date.now)})
+##### ORRRR...
 
+Vacation.create({date_taken: "02/13/01", days_left: 10 })
 
+Vacation.create({date_taken: "04/09/11", days_left: 08 })
+
+Vacation.create({date_taken: "11/23/11", days_left: 10 })
+
+Vacation.create({date_taken: "10/19/14", days_left: 03 })
+
+Vacation.create({date_taken: "04/13/01", days_left: 10 })
+
+Vacation.create({date_taken: "09/30/10", days_left: 07 })
+
+Vacation.create({date_taken: "02/17/15", days_left: 14 })
+
+Vacation.create({date_taken: "10/01/01", days_left: 12 })
+
+Vacation.create({date_taken: "04/09/11", days_left: 08 })
+
+Vacation.create({date_taken: "05/20/07", days_left: 10 })
+
+Vacation.create({date_taken: "02/20/05", days_left: 03 })
+
+Vacation.create({date_taken: "12/13/01", days_left: 10 })
+
+Vacation.create({date_taken: "08/30/13", days_left: 07 })
+
+Vacation.create({date_taken: "02/01/11", days_left: 14 })
+
+Vacation.create({date_taken: "10/14/09", days_left: 12 })
